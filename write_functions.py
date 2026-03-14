9fc13dccfb6367355a32e55f514638e8c962c3f6
isaac quigley <eyesaq1@gmail.com>
2026-03-14 22:57:00 +0000

    first rough mock-up

---
+++ write_functions.py
@@ -0,0 +1,38 @@
+import os
+import json
+
+# utility functions for the SSH GUI application
+
+# creates a batch file to ssh into the device
+def write(ip, user, name):
+    path = os.path.join("Devices", f"{name}.bat")
+    with open(path, "w") as f:
+        f.write(f"ssh {user}@{ip}")
+    print(f"Batch file {path} created with content: ssh {user}@{ip}")
+
+# opens the batch file to ssh into the device
+
+def startssh(name):
+    path = os.path.join("Devices", f"{name}.bat")
+    os.startfile(path)
+
+# writes device info to devices.json
+def write_name_json(data, filename="devices.json"):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+# writes ip to ips.json
+def write_ip_json(data2, filename="ips.json"):
+    with open(filename, "w") as f:
+        json.dump(data2, f, indent=4)
+
+#writes usernames to usernames.json
+def write_username_json(data3, filename="usernames.json"):
+    with open(filename, "w") as f:
+        json.dump(data3, f, indent=4)
+
+#writes pwds to passwords.json
+def write_password_json(data4, filename="passwords.json"):
+    with open(filename, "w") as f:
+        json.dump(data4, f, indent=4 )
+
