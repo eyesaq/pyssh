aac5b6f89bd3d675f1f752d210c6fc017b76b544
M0U5S3 <dariushwalker28@gmail.com>
2026-03-19 22:58:38 +0000

    Add edit device dialog

---
+++ app/dialogs/edit_device.py
@@ -0,0 +1,18 @@
+# Local application imports
+from app.dialogs.base_device_input import BaseDeviceInput
+
+class EditDeviceDialog(BaseDeviceInput):
+    def __init__(self, parent, app, ip_address):
+        self._parent = parent
+        self._app = app
+
+        self.old_state = self._app.database.get_device_info_by_ip(ip_address)
+
+        defaults = {
+            "ip_address": self.old_state[0],
+            "device_name": self.old_state[1],
+            "username": self.old_state[2],
+            "password": self.old_state[3],
+        }
+
+        super().__init__(self._parent, self._app.database.update_device_by_ip, 'Edit Device', defaults=defaults)
