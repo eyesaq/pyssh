0b23e7be7b4c5e1381541ad39231a4d7f3d00af2
M0U5S3 <dariushwalker28@gmail.com>
2026-03-24 21:19:43 +0000

    Added delete icon

--- app/storage/icons.py
+++ app/storage/icons.py
@@ -1,13 +1,17 @@
 from PIL import Image
 import customtkinter as ctk
+from pathlib import Path
 
 class Icons:
     def __init__(self):
-        self.edit_button = self._load("data/icons/edit_button.png")
+        self._icons_path = Path('data/icons')
 
-    def _load(self, path, size=(20, 20)):
+        self.edit_button = self._load('edit_button.png')
+        self.delete_button = self._load('delete_button.png')
+
+    def _load(self, file_name, size=(10, 10)):
         return ctk.CTkImage(
-            light_image=Image.open(path),
-            dark_image=Image.open(path),
+            light_image=Image.open(self._icons_path/file_name),
+            dark_image=Image.open(self._icons_path/file_name),
             size=size
         )
